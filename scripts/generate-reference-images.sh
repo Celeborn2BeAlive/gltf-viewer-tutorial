c3eafe8df39f83fe2b4e33f96d5c7de3a71aeaab
Celeborn2BeAlive <laurent.noel.c2ba@gmail.com>
2020-01-21 16:43:28 +0100

    Add some scripts for testing

---
+++ scripts/generate-reference-images.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SCRIPT_DIR=`dirname "$0"`
+SRC_DIR=$SCRIPT_DIR/..
+source $SCRIPT_DIR/env.env
+
+COMMIT_SLUG=`git rev-parse HEAD`
+REF_IMG_DIR=$REF_IMG_DIR/$COMMIT_SLUG
+
+mkdir -p $REF_IMG_DIR
+echo $REF_IMG_DIR
+source $SCRIPT_DIR/generate-reference-images-cmd.sh
---
+++ scripts/generate-reference-script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SCRIPT_DIR=`dirname "$0"`
+SRC_DIR=$SCRIPT_DIR/..
+source $SCRIPT_DIR/env.env
+
+for d in ${GLTF_MODELS_PATH}/*/ ; do
+  MODEL_NAME=`basename $d`
+  PATH_TO_GLTF_FILE=$MODEL_NAME/'glTF'/$MODEL_NAME.gltf
+  echo "gltf-viewer viewer \$GLTF_MODELS_PATH/$PATH_TO_GLTF_FILE --output \$REF_IMG_DIR/$MODEL_NAME.png"
+done
