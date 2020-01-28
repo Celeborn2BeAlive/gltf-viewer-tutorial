806050d63cf26b36f2b220db4275f5704045e615
Amel BEN HAMOUDA <amel@debian>
2020-01-28 14:10:01 +0100

    TODO Better default camera

--- apps/gltf-viewer/ViewerApplication.cpp
+++ apps/gltf-viewer/ViewerApplication.cpp
@@ -170,26 +170,35 @@ int ViewerApplication::run() {
     const auto modelViewMatrixLocation = glGetUniformLocation(glslProgram.glId(), "uModelViewMatrix");
     const auto normalMatrixLocation = glGetUniformLocation(glslProgram.glId(), "uNormalMatrix");
 
-    // Build projection matrix
-    auto maxDistance = 500.f; // TODO use scene bounds instead to compute this
-    maxDistance = maxDistance > 0.f ? maxDistance : 100.f;
-    const auto projMatrix = glm::perspective(70.f, float(m_nWindowWidth) / m_nWindowHeight, 0.001f * maxDistance, 1.5f * maxDistance);
-
-    // TODO Implement a new CameraController model and use it instead. Propose the
-    // choice from the GUI
-    FirstPersonCameraController cameraController{ m_GLFWHandle.window(), 0.5f * maxDistance };
-    if (m_hasUserCamera) {
-        cameraController.setCamera(m_userCamera);
-    } 
-    else {
-        // TODO Use scene bounds to compute a better default camera
-        cameraController.setCamera(Camera{ glm::vec3(0, 0, 0), glm::vec3(0, 0, -1), glm::vec3(0, 1, 0) });
-    }
-
     tinygltf::Model model;
     // TODO Loading the glTF file
     if (!loadGltfFile(model)) {
         return -1;
+    }
+
+    glm::vec3 bboxMin, bboxMax;
+    computeSceneBounds(model, bboxMin, bboxMax);
+
+    // // Build projection matrix
+    // auto maxDistance = 500.f; // TODO use scene bounds instead to compute this
+    // maxDistance = maxDistance > 0.f ? maxDistance : 100.f;
+    const auto diag = bboxMax - bboxMin;
+    auto maxDistance = glm::length(diag);
+    const auto projMatrix = glm::perspective(70.f, float(m_nWindowWidth) / m_nWindowHeight, 0.001f * maxDistance, 1.5f * maxDistance);
+
+    // TODO Implement a new CameraController model and use it instead. Propose the
+    // choice from the GUI
+    FirstPersonCameraController cameraController{ m_GLFWHandle.window(), 0.5f * maxDistance};
+    if (m_hasUserCamera) {
+        cameraController.setCamera(m_userCamera);
+    } 
+    else {
+        const auto center = 0.5f * (bboxMax + bboxMin);
+        const auto up = glm::vec3(0, 1, 0);
+        const auto eye = diag.z > 0 ? center + diag : center + 2.f * glm::cross(diag, up);
+        // TODO Use scene bounds to compute a better default camera
+        //cameraController.setCamera(Camera{ glm::vec3(0, 0, 0), glm::vec3(0, 0, -1), glm::vec3(0, 1, 0) });
+        cameraController.setCamera(Camera{eye, center, up});
     }
 
     // TODO Creation of Buffer Objects
@@ -267,7 +276,7 @@ int ViewerApplication::run() {
         }
     };
 
-    // Loop until the user closes the window
+    //TODO Render to image
     if (!(m_OutputPath.empty())) {
         const auto nbComponent = 3;
         std::vector<unsigned char> pixels(m_nWindowWidth * m_nWindowHeight * nbComponent);
@@ -282,6 +291,7 @@ int ViewerApplication::run() {
         return 0;
     }
 
+    // Loop until the user closes the window
     for (auto iterationCount = 0u; !m_GLFWHandle.shouldClose(); ++iterationCount) {
         const auto seconds = glfwGetTime();
         const auto camera = cameraController.getCamera();
