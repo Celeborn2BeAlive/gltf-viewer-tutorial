f10186ac0f48e8308bbe73dd8016a122e7f117bf
Laurent NOEL <laurent.noel.c2ba@gmail.com>
2020-02-05 13:17:12 +0100

    TrackballCamera: usage in run()

--- apps/gltf-viewer/ViewerApplication.cpp
+++ apps/gltf-viewer/ViewerApplication.cpp
@@ -53,7 +53,7 @@ int ViewerApplication::run()
 
   // TODO Implement a new CameraController model and use it instead. Propose the
   // choice from the GUI
-  FirstPersonCameraController cameraController{
+  TrackballCameraController cameraController{
       m_GLFWHandle.window(), 0.5f * maxDistance};
   if (m_hasUserCamera) {
     cameraController.setCamera(m_userCamera);
