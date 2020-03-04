feeb1d4a9c668fa3672280cb4f16f75c28744906
Nopharut CHADEE <nchadee@etud.u-pem.fr>
2020-03-04 02:03:52 +0100

    Drawing the scene, part 3: Render to image

--- apps/gltf-viewer/ViewerApplication.cpp
+++ apps/gltf-viewer/ViewerApplication.cpp
@@ -10,6 +10,7 @@
 
 #include "utils/cameras.hpp"
 #include "utils/gltf.hpp"
+#include "utils/images.hpp"
 
 #include <stb_image_write.h>
 #include <tiny_gltf.h>
@@ -219,6 +220,17 @@ int ViewerApplication::run()
     	}
     }
   };
+
+  if(!m_OutputPath.empty()) {
+  	std::vector<unsigned char> pixels(m_nWindowWidth * m_nWindowHeight * 3);
+  	renderToImage(m_nWindowWidth, m_nWindowHeight, 3, pixels.data(), [&](){
+  		drawScene(cameraController.getCamera());
+  	});
+  	flipImageYAxis(m_nWindowWidth, m_nWindowHeight, 3, pixels.data());
+  	const std::string strPath = m_OutputPath.string();
+  	stbi_write_png(strPath.c_str(), m_nWindowWidth, m_nWindowHeight, 3, pixels.data(), 0);
+  	return 0;
+  }
 
   // Loop until the user closes the window
   for (auto iterationCount = 0u; !m_GLFWHandle.shouldClose();
